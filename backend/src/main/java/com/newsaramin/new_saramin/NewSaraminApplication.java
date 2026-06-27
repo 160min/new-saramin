a7d13aa698fc84aaece9d03175fc10d0055a1430
hw4ni <wjdEjttkfwk@icloud.com>
2026-06-27 14:42:25 +0900

    [BACK-401] chore: 백엔드 환경 세팅

---
+++ backend/src/main/java/com/newsaramin/new_saramin/NewSaraminApplication.java
@@ -0,0 +1,13 @@
+package com.newsaramin.new_saramin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSaraminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSaraminApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/newsaramin/new_saramin/NewSaraminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsaramin.new_saramin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSaraminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
